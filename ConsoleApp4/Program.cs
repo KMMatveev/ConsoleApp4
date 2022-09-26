9b8cd06614c679d3ad3922dbc2606c3e2a036eed
KMMatveev <KMMatveev@stud.kpfu.ru>
2022-09-26 15:10:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿double x, y;
+Console.WriteLine("Write X, then Y");
+x=double.Parse(Console.ReadLine());
+y=double.Parse(Console.ReadLine());
+
+if ((x<=1)&&(x>=-1)&& (y <= 1) && (y >= -1))
+{
+    if (y<=-Math.Abs(x)+1)
+    {
+        if (y>=-x*x)
+        { Console.WriteLine("True"); }
+        else
+        { Console.WriteLine("False"); }
+    }
+    else
+    { Console.WriteLine("False"); }
+}
+else
+{ Console.WriteLine("False"); }
